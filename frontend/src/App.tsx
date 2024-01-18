bc5e9c3cca550e89e5d8cafa049238bc3f8f6051
chaoticsoooul <natal6ie@gmail.com>
2024-01-18 21:50:24 +0800

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
 import { ConnectKitButton } from 'connectkit';
+import './style.css';
 
 function App() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-      }}
-    >
-      <ConnectKitButton />
+    <div>
+      <head>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap');
+      </style>  
+      </head>
+      <header>
+        <h1 className='project-name'>LFGHO</h1>
+        <nav>
+        <ul>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>Create</a></li>
+        </ul>
+      </nav>  
+      <div className='connect-button'>
+        <ConnectKitButton />
+        </div>
+      </header>
+      
+      
     </div>
   );
 }
